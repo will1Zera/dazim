2db43ec4b1c4188244b30e89660c54be0a89621a
William Bierhals <willambierhals>
2024-03-26 21:37:19 -0300

    :bricks: Adiciona autoload e psr-4

---
+++ api/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '3a8a1873055ab12a6dfd02701855c1acf4a5cc8b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '3a8a1873055ab12a6dfd02701855c1acf4a5cc8b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
